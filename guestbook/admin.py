09911149c534d9081d5e4b96f7d489c5371af334
spark131072 <spark131072@gmail.com>
2018-11-19 17:44:03 +0800

    Initial commit

---
+++ guestbook/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from guestbook.models import TextMessage
+
+# Register your models here.
+
+admin.site.register(TextMessage)
---
+++ helloworld/urls.py
@@ -0,0 +1,47 @@
+# """helloworld URL Configuration
+#
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/2.1/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+"""
+131072
+121018
+urls.py
+
+
+
+
+"""
+
+
+# =============
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('base/', views.base, name='base'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('guestbook/', views.guestbook, name='guestbook'),
+    path('personal_history/', views.personal_history, name='personal_history'),
+    path('account/', include('account.urls')),
+    path('account/', include('django.contrib.auth.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
