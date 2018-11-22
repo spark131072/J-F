0a4eaa0f5b660861fc02a7de887463262ad152e1
spark131072 <spark131072@gmail.com>
2018-11-23 01:38:02 +0800

    221118_06_complete customizing signup form

--- account/forms.py
+++ account/forms.py
@@ -13,14 +13,24 @@ account/forms.py
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.db import models
+
 
 
 class SignUpForm(UserCreationForm):
+    YEARS= [x for x in range(1936,2026)]
+    GENDER_CHOICES = (
+            ('M', 'Male'),
+            ('F', 'Female'),
+        )
+
+
     first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
+    birth_date = forms.DateField(label='Birth Date', widget=forms.SelectDateWidget(years=YEARS))
+    gender = forms.ChoiceField(choices = GENDER_CHOICES, required=True, help_text='if you are Male please type \"M\", if you are female please type \"F\".')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'gender', 'birth_date', 'email', 'password1', 'password2')
--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+"""
+131072
+231118 - initial
+account\forms.py
 
+
+
+"""
+
+
+# =============
+
+from django.db import models
+import choices
 # Create your models here.
