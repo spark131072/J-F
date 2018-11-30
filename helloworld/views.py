aa1d73fb5afb0507fe76e41b64cd4ebd6a4c440b
spark131072 <spark131072@gmail.com>
2018-11-30 13:54:49 +0800

    debug

--- helloworld/views.py
+++ helloworld/views.py
@@ -35,12 +35,13 @@ def geo(request):
         _lng = 0.000000
 
         if request.method == 'POST':
-            json_receive = json.loads(request.body)
+            _lat = request.POST.get('lat')
+            # json_receive = json.loads(request.body)
 
             # _lat = json_receive.lat
             # _lng = json_receive['lng']
 
-            print(json_receive)
+            print(_lat)
 
         if _lat != 0.000000 and _lng != 0.000000 and _lat is not None and _lng is not None:
             if Geo.objects.filter(user=_user).count() == 0:
