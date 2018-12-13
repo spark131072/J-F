44e95f6776cffdb2b004a9832151f3b8234bff17
spark131072 <spark131072@gmail.com>
2018-12-14 01:33:41 +0800

    debug

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: gunicorn helloworld.wsgi
-web: daphne my_project.asgi:application --port $port --bind 0.0.0.0
+web: daphne helloworld.asgi:application --port $port --bind 0.0.0.0
 worker: python manage.py runworker channels -v2
--- helloworld/asgi.py
+++ helloworld/asgi.py
@@ -1,7 +1,5 @@
 import os
-import django
-from channels.routing import get_default_application
+import channels.asgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld.settings")
-django.setup()
-application = get_default_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
+channel_layer = channels.asgi.get_channel_layer()
